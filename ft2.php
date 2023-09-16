1f08648d75046c5071162434e13aa6af7a69b2a5
NAITOH Jun <naitoh@gmail.com>
2023-09-16 22:48:27 +0900

    support PHP8.0
    [Why]
    - PHP7.4: Warning: Array and string offset access syntax with curly braces is deprecated
    - PHT8.0: Fatal error: Array and string offset access syntax with curly braces is no longer supported

--- ft2.php
+++ ft2.php
@@ -662,7 +662,7 @@ function ft_do_action() {
  */
 function ft_get_bytes($val) {
 	$val = trim($val);
-	$last = strtolower($val{strlen($val)-1});
+	$last = strtolower($val[strlen($val)-1]);
 	switch($last) {
 		// The 'G' modifier is available since PHP 5.1.0
 		case 'g':
