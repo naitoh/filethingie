ab6d1f4df09ca4409b23d0e8f0f7d48c9bbc1500
NAITOH Jun <naitoh@gmail.com>
2023-09-16 22:49:45 +0900

    Support PHP7.1
    [Why]
    Warning: A non-numeric value encountered
    
    See: https://www.php.net/manual/en/migration71.other-changes.php
    
    > Notices and warnings on arithmetic with invalid strings
    
    > New E_WARNING and E_NOTICE errors have been introduced when invalid strings are coerced using operators expecting numbers (+ - * / ** % << >> | & ^) or their assignment equivalents.
    > An E_NOTICE is emitted when the string begins with a numeric value but contains trailing non-numeric characters, and an E_WARNING is emitted when the string does not contain a numeric value.

--- ft2.php
+++ ft2.php
@@ -663,6 +663,7 @@ function ft_do_action() {
 function ft_get_bytes($val) {
 	$val = trim($val);
 	$last = strtolower($val[strlen($val)-1]);
+	$val = (int)$val;
 	switch($last) {
 		// The 'G' modifier is available since PHP 5.1.0
 		case 'g':
